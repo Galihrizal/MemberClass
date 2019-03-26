67750bcc0bdc06c2edd09ee8d7c6d928e81563d0
Galihrizal <galih.2301@students.amikom.ac.id>
2019-03-26 14:50:14 +0700

    MemberClass

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace class_dan_properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari class taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            if (taxi.OnDuty == true)
+            {
+                taxi.PickUpPassenger();
+            }
+            else
+            {
+                taxi.DropOffPassenger();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace class_dan_properties
+{
+    class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name         : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty ? "Yes":"No");
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n\n{0} sedang menjemput penumpang\n", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("\n\n{0} selesai mengantar penumpang\n", DriverName);
+        }
+    }
+}
